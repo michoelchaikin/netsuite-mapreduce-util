e92a923b633364e850beb5029939554697f83f04
Michoel Chaikin <micholi@gmail.com>
2020-06-28 11:56:17 +1000

    fixes to create-spa-netsuite
    from author (@mayerlench), should be fixed in new version

--- client/postbuild.js
+++ client/postbuild.js
@@ -1,4 +1,11 @@
-const mv = require('mv')
-
-const folderName  = 'mru o5dYJbA1LN'
-mv("dist", `../sdf/dist/FileCabinet/SuiteScripts/mapreduce-util/${folderName}`, function(err, succ) {})
+const mv = require('mv');
+const folderName = 'mru o5dYJbA1LN';
+mv(
+  'dist',
+  `../sdf/src/FileCabinet/SuiteScripts/mapreduce-util/${folderName}`,
+  function(err, succ) {
+    if (err) console.log(err);
+    else
+      console.log('\nBuild completed!\nIn the sdf folder run "npm run deploy"');
+  }
+);
