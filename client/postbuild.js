72c30fd39499b198169b7c357400007bffb3b7b8
Michoel Chaikin <michoel.chaikin@carsales.com.au>
2020-06-30 21:09:05 +1000

    fixes to client postbuild
    From create-spa-netsuite author
    See https://github.com/finitydevs/create-spa-netsuite/issues/4

--- client/postbuild.js
+++ client/postbuild.js
@@ -1,11 +1,13 @@
 const mv = require('mv');
+const rimraf = require('rimraf');
+
 const folderName = 'mru o5dYJbA1LN';
-mv(
-  'dist',
-  `../sdf/src/FileCabinet/SuiteScripts/mapreduce-util/${folderName}`,
-  function(err, succ) {
+const folderPath = `../sdf/src/FileCabinet/SuiteScripts/mapreduce-util/${folderName}`;
+
+rimraf(folderPath, () => {
+  mv('dist', folderPath, function(err, succ) {
     if (err) console.log(err);
     else
       console.log('\nBuild completed!\nIn the sdf folder run "npm run deploy"');
-  }
-);
+  });
+});
